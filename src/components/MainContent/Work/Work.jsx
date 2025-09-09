2aa0612fc100f484bcc783b6a31bc25e615f4445
hadamas <alanis.hadama@gmail.com>
2025-09-09 09:47:31 -0300

    add: Work info and tools

--- src/components/MainContent/Work/Work.jsx
+++ src/components/MainContent/Work/Work.jsx
@@ -1,14 +1,74 @@
 import "./Work.css";
+import { useState } from "react";
 
 function Work() {
+  const [developmentList] = useState([
+    "JavaScript",
+    "React",
+    "Vue",
+    "HTML/CSS",
+    "Python",
+    "C",
+    "C++",
+    "ThreeJS",
+  ]);
+  const [toolsList] = useState([
+    "Git",
+    "VS Code",
+    "Docker",
+    "Node.js",
+    "PostgresSQL",
+    "Insomnia",
+    "Blender",
+    "Qlinkview",
+  ]);
+
   return (
     <div className="page-section">
       <h2>experiência profissional</h2>
-      <p>
-        Currently, I am a student with no professional experience listed yet. I
-        am eager to start my career in software development and will update this
-        section with my future achievements!
-      </p>
+      <div className="work-section">
+        <div className="work-experience">
+          <h3>Expêriencia Profissional</h3>
+          <ul>
+            <li>
+              <h6>Estágiaria em desenvolvimento de software</h6>
+              <p>09/2024 - Fundação Petrobras de Seguridade Social - PETROS</p>
+              <p>Setor de Investimentos</p>
+            </li>
+            <li>
+              <h6>Jovem Aprendiz</h6>
+              <p>02/2020 - 12/2020 ELETRONUCLEAR S.A.</p>
+              <p>Empresa operária e construtora de usinas termonucleares no Brasil.</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="skill-container">
+          <div className="development">
+            <h3>Desenvolvimento</h3>
+            <div className="work-button-itens">
+              {developmentList.map((skill, index) => (
+                <button key={index} className="work-button">
+                  {skill}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="tools">
+            <h3>Ferramentas</h3>
+            <div className="work-button-itens">
+              {toolsList.map((tool, index) => (
+                <button key={index} className="work-button">
+                  {tool}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="projects"></div>
+      </div>
+      <p className="small-msg">Veja mais no meu <a href="https://github.com/hadamas?tab=repositories" target="_blank">Github</a></p>
     </div>
   );
 }
